e7959d9e70e34a01d659b30a878d79a541102a06
hai_pham_minh <hai.phamminh86@gmail.com>
2019-10-04 17:46:46 +0700

    phone billing - add new class CallDuration

--- src/main/java/com/usunified/PhoneBilling.java
+++ src/main/java/com/usunified/PhoneBilling.java
@@ -1,6 +1,5 @@
 package com.usunified;
 
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,47 +7,59 @@ import java.util.Map;
 
 public class PhoneBilling {
     private static final int FIVE_MINUTES_IN_SEC = 5 * 60;
+    private static final int SECONDS_PER_HOUR = 1 * 60 * 60;
+    private static final int SECONDS_PER_MINUTE = 1 * 60;
 
     public static void main(String[] args) {
-//        String S = "00:01:07,400-234-090 00:05:01,701-080-080 00:05:00,400-234-090";
-        String S = "00:01:07,400-234-090 00:05:01,701-080-080 00:01:06,701-080-080 00:05:00,400-234-090";
-//        String S = "07:01:07,400-234-090 10:05:01,701-080-080 00:05:00,400-234-090";
+//        String S = "00:01:07,400-234-090 00:05:01,701-080-080 00:05:00,400-234-090"; // 900
+        String S = "00:01:07,400-234-090 00:05:01,701-080-080 00:00:01,601-080-080 00:01:06,701-080-080 00:05:00," +
+                "400-234-090"; // 951
+//        String S = "07:01:07,400-234-090 10:05:01,701-080-080 00:05:00,400-234-090"; // 64050
+//        String S = "00:01:07,400-234-090 00:05:00,400-234-090"; // 951
+//        String S = "00:01:07,400-234-090"; // 201
+//        String S = ""; // 0
         System.out.println(solution(S));
     }
 
     static int solution(String S) {
+        if (S.isEmpty()) return 0;
+
         String[] phoneCalls = S.split(" ");
-        Map<String, List<LocalTime>> phoneLogsMap = new HashMap<>();
-        long longestTotalDuration = 0;
+        Map<String, List<CallDuration>> phoneLogsMap = new HashMap<>();
+
         int totalAmount = 0;
         for (String call : phoneCalls) {
             // parse call duration and phone number, then add all durations to the map
             String[] durationAndPhoneNumber = call.split(",");
             String phoneNumber = durationAndPhoneNumber[1];
             String callDuration = durationAndPhoneNumber[0];
-            LocalTime duration = LocalTime.parse(callDuration);
-            List<LocalTime> durationPerNumber = phoneLogsMap.getOrDefault(phoneNumber, new ArrayList<>());
+            CallDuration duration = CallDuration.parse(callDuration);
+            List<CallDuration> durationPerNumber = phoneLogsMap.getOrDefault(phoneNumber, new ArrayList<>());
             durationPerNumber.add(duration);
             phoneLogsMap.put(phoneNumber, durationPerNumber);
-
-            // find the longest total duration
-            long totalSecPerNumber = computeTotalSecPerNumber(durationPerNumber);
-            longestTotalDuration = Math.max(longestTotalDuration, totalSecPerNumber);
 
             // compute amount
             totalAmount += compute(duration);
         }
 
+        // in case there is only one phone number logged, it must pay
+        if (phoneLogsMap.size() == 1) {
+            return totalAmount;
+        }
+
+        long longestTotalDuration = 0;
+        for (Map.Entry<String, List<CallDuration>> e : phoneLogsMap.entrySet()) {
+            // find the longest total duration
+            long totalSecPerNumber = compute(e.getValue());
+            longestTotalDuration = Math.max(longestTotalDuration, totalSecPerNumber);
+        }
+
         // find the smallest phone number that shares the longest total duration
         String freePhoneNumber = "";
-        int smallestPhoneNumber = Integer.MAX_VALUE;
-        for (Map.Entry<String, List<LocalTime>> e : phoneLogsMap.entrySet()) {
-            long totalSecPerNumber = computeTotalSecPerNumber(e.getValue());
+        for (Map.Entry<String, List<CallDuration>> e : phoneLogsMap.entrySet()) {
+            long totalSecPerNumber = compute(e.getValue());
             if (totalSecPerNumber == longestTotalDuration) {
-                int number = Integer.parseInt(e.getKey()
-                                               .split("-")[0]);
-                if (number < smallestPhoneNumber) {
-                    smallestPhoneNumber = number;
+                if (freePhoneNumber.isEmpty() || freePhoneNumber.compareToIgnoreCase(e.getKey()) > 0) {
                     freePhoneNumber = e.getKey();
                 }
             }
@@ -60,29 +71,62 @@ public class PhoneBilling {
         return totalAmount - freeAmount;
     }
 
-    static long computeTotalSecPerNumber(List<LocalTime> localTimes) {
-        return localTimes.stream()
-                         .mapToLong(LocalTime::toSecondOfDay)
-                         .sum();
+    static int compute(List<CallDuration> callDurations) {
+        return callDurations.stream()
+                            .mapToInt(PhoneBilling::compute)
+                            .sum();
     }
 
-    static int compute(List<LocalTime> localTimes) {
-        return localTimes.stream()
-                         .mapToInt(PhoneBilling::compute)
-                         .sum();
-    }
-
-    static int compute(LocalTime lt) {
+    static int compute(CallDuration callDuration) {
         int amount = 0;
-        int sec = lt.toSecondOfDay();
+        int sec = callDuration.toSecond();
         if (sec < FIVE_MINUTES_IN_SEC) {
             amount += sec * 3;
         } else if (sec == FIVE_MINUTES_IN_SEC) {
             amount += 5 * 150;
         } else {
-            int addedMin = lt.getSecond() > 0 ? 1 : 0;
-            amount += (lt.getHour() * 60 + lt.getMinute() + addedMin) * 150;
+            int addedMin = callDuration.getSecond() > 0 ? 1 : 0;
+            amount += (callDuration.getHour() * 60 + callDuration.getMinute() + addedMin) * 150;
         }
         return amount;
+    }
+
+    static class CallDuration {
+        private final int hour;
+        private final int minute;
+        private final int second;
+
+        public CallDuration(int hour, int minute, int second) {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public int toSecond() {
+            int total = hour * SECONDS_PER_HOUR;
+            total += minute * SECONDS_PER_MINUTE;
+            total += second;
+            return total;
+        }
+
+        public int getHour() {
+            return hour;
+        }
+
+        public int getMinute() {
+            return minute;
+        }
+
+        public int getSecond() {
+            return second;
+        }
+
+        public static CallDuration parse(String text) {
+            String[] parsedText = text.split(":");
+            int hour = Integer.valueOf(parsedText[0]);
+            int minute = Integer.valueOf(parsedText[1]);
+            int second = Integer.valueOf(parsedText[2]);
+            return new CallDuration(hour, minute, second);
+        }
     }
 }
