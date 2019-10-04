b4f2da8df079fb47f1092c5a614adf5a89a55992
hai_pham_minh <hai.phamminh86@gmail.com>
2019-10-04 16:23:08 +0700

    phone billing

--- src/main/java/com/usunified/PhoneBilling.java
+++ src/main/java/com/usunified/PhoneBilling.java
@@ -10,64 +10,66 @@ public class PhoneBilling {
     private static final int FIVE_MINUTES_IN_SEC = 5 * 60;
 
     public static void main(String[] args) {
-//        String S = "00:01:07,400-234-090 00:05:01,701-080-080 00:01:06,701-080-080 00:05:00,400-234-090";
-        String S = "07:01:07,400-234-090 10:05:01,701-080-080 00:05:00,400-234-090";
+//        String S = "00:01:07,400-234-090 00:05:01,701-080-080 00:05:00,400-234-090";
+        String S = "00:01:07,400-234-090 00:05:01,701-080-080 00:01:06,701-080-080 00:05:00,400-234-090";
+//        String S = "07:01:07,400-234-090 10:05:01,701-080-080 00:05:00,400-234-090";
         System.out.println(solution(S));
     }
 
     static int solution(String S) {
-        Map<String, List<LocalTime>> callMap = parse(S);
-        long maxSeconds = 0;
-        int minNumber = Integer.MAX_VALUE;
-        int total = 0;
-        for (Map.Entry<String, List<LocalTime>> e : callMap.entrySet()) {
-            long toSeconds = toSeconds(e.getValue());
-            maxSeconds = Math.max(maxSeconds, toSeconds);
-            total += e.getValue()
-                      .stream()
-                      .mapToInt(lt -> compute(lt))
-                      .sum();
+        String[] phoneCalls = S.split(" ");
+        Map<String, List<LocalTime>> phoneLogsMap = new HashMap<>();
+        long longestTotalDuration = 0;
+        int totalAmount = 0;
+        for (String call : phoneCalls) {
+            // parse call duration and phone number, then add all durations to the map
+            String[] durationAndPhoneNumber = call.split(",");
+            String phoneNumber = durationAndPhoneNumber[1];
+            String callDuration = durationAndPhoneNumber[0];
+            LocalTime duration = LocalTime.parse(callDuration);
+            List<LocalTime> durationPerNumber = phoneLogsMap.getOrDefault(phoneNumber, new ArrayList<>());
+            durationPerNumber.add(duration);
+            phoneLogsMap.put(phoneNumber, durationPerNumber);
+
+            // find the longest total duration
+            long totalSecPerNumber = computeTotalSecPerNumber(durationPerNumber);
+            longestTotalDuration = Math.max(longestTotalDuration, totalSecPerNumber);
+
+            // compute amount
+            totalAmount += compute(duration);
         }
-        String freeCall = "";
-        for (Map.Entry<String, List<LocalTime>> e : callMap.entrySet()) {
-            long toSeconds = toSeconds(e.getValue());
-            if (toSeconds == maxSeconds) {
-                int number = parse3FirstNumberOfPhoneNumber(e.getKey());
-                if (number < minNumber) {
-                    minNumber = number;
-                    freeCall = e.getKey();
+
+        // find the smallest phone number that shares the longest total duration
+        String freePhoneNumber = "";
+        int smallestPhoneNumber = Integer.MAX_VALUE;
+        for (Map.Entry<String, List<LocalTime>> e : phoneLogsMap.entrySet()) {
+            long totalSecPerNumber = computeTotalSecPerNumber(e.getValue());
+            if (totalSecPerNumber == longestTotalDuration) {
+                int number = Integer.parseInt(e.getKey()
+                                               .split("-")[0]);
+                if (number < smallestPhoneNumber) {
+                    smallestPhoneNumber = number;
+                    freePhoneNumber = e.getKey();
                 }
             }
         }
-        int freePrice = callMap.get(freeCall)
-                               .stream()
-                               .mapToInt(lt -> compute(lt))
-                               .sum();
-        return total - freePrice;
+
+        // re-compute the free amount for the free promotion
+        int freeAmount = compute(phoneLogsMap.get(freePhoneNumber));
+
+        return totalAmount - freeAmount;
     }
 
-    static int parse3FirstNumberOfPhoneNumber(String phoneNumber) {
-        return Integer.parseInt(phoneNumber.split("-")[0]);
-    }
-
-    static long toSeconds(List<LocalTime> localTimes) {
+    static long computeTotalSecPerNumber(List<LocalTime> localTimes) {
         return localTimes.stream()
                          .mapToLong(LocalTime::toSecondOfDay)
                          .sum();
     }
 
-    static Map<String, List<LocalTime>> parse(String S) {
-        String[] phoneCalls = S.split(" ");
-        Map<String, List<LocalTime>> callMap = new HashMap<>();
-        for (String call : phoneCalls) {
-            String[] durationAndPhoneNumber = call.split(",");
-            String phoneNumber = durationAndPhoneNumber[1];
-            String callDuration = durationAndPhoneNumber[0];
-            List<LocalTime> durationPerNumber = callMap.getOrDefault(phoneNumber, new ArrayList<>());
-            durationPerNumber.add(LocalTime.parse(callDuration));
-            callMap.put(phoneNumber, durationPerNumber);
-        }
-        return callMap;
+    static int compute(List<LocalTime> localTimes) {
+        return localTimes.stream()
+                         .mapToInt(PhoneBilling::compute)
+                         .sum();
     }
 
     static int compute(LocalTime lt) {
